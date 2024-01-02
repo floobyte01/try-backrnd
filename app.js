095e4c74f8ccf8845cc03ee3e578e9a6a9dbc828
rajivsuting <rajivsuting16@gmail.com>
2024-01-02 12:33:24 +0530

    cors updated

--- app.js
+++ app.js
@@ -22,7 +22,13 @@ const cors = require("cors");
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500/",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 app.use(
   fileUpload({
     useTempFiles: true,
